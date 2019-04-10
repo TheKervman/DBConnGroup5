b832842d013f169dd888608edf8ee1eec0f9cb7c
Kevin <thekervman@Kevins-MacBook-Pro.local>
2019-04-10 19:21:14 -0400

    moved files to extra folder

--- Milestone3/gui/MainPanel.java
+++
@@ -1,7 +0,0 @@
-package gui;
-
-import javax.swing.*;
-import java.awt.*;
-
-
-// Set up the panel and have the login button link to at least return some information on this panel to show that it links
