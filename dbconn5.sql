86341326170ec9cc140dc18186c0447f059c7e1d
Kevin <thekervman@Kevins-MacBook-Pro.local>
2019-04-25 19:18:56 -0400

    got the sql working

--- dbconn5.sql
+++ dbconn5.sql
@@ -18,10 +18,6 @@ CREATE TABLE Faculty(
 	interviewID int,
 	presentationID int,
 	PRIMARY KEY (userID),
-	CONSTRAINT FK_DepartmentCode FOREIGN KEY (departmentCode) REFERENCES Department(departmentCode),
-	CONSTRAINT FK_UserID FOREIGN KEY (userID) REFERENCES Users(userID),
-	CONSTRAINT FK_InterviewID FOREIGN KEY (interviewID) REFERENCES Interview(interviewID),
-	CONSTRAINT FK_PresentationID FOREIGN KEY (presentationID) REFERENCES Presentation(presentationID),
     UNIQUE (facName)
 );
 
@@ -30,12 +26,10 @@ CREATE TABLE Student(
 	studName varchar(25),
 	email varchar(35),
 	topicID int,
-	PRIMARY KEY (userID),
-	FOREIGN KEY (userID) REFERENCES Users(userID),
-            FOREIGN KEY (topicID) REFERENCES Topic(topicID)
+	PRIMARY KEY (userID)
 );
 
-CREATE TABLE  Department(
+CREATE TABLE Department(
 	departmentID int,
 	departmentName varchar(25),
 	departmentCode char(3),
@@ -43,30 +37,42 @@ CREATE TABLE  Department(
     UNIQUE (departmentCode)
 );
 
-CREATE TABLE  Topic(
+CREATE TABLE Topic(
 	topicID int AUTO_INCREMENT,
 	topicName varchar(25),
 	facID int,
 	tags char(4),
 	topicDescription varchar(250),
-	PRIMARY KEY (topicID),
-	FOREIGN KEY (facID) REFERENCES Faculty(facID)
+	PRIMARY KEY (topicID)
 );
 
-CREATE TABLE  Interview(
+CREATE TABLE Interview(
 	interviewID int AUTO_INCREMENT,
 	interviewName varchar(25),
 	interviewDate date,
 	facName varchar(25),
-	PRIMARY KEY(interviewID),
-	FOREIGN KEY (facName) REFERENCES Faculty(facName)
+	PRIMARY KEY(interviewID)
 );
 
-CREATE TABLE  Presentation(
+CREATE TABLE Presentation(
 	presentationID int AUTO_INCREMENT,
 	presentationName varchar(25),
 	presentationDate date,
 	facName varchar(25),
-	PRIMARY KEY(presentationID),
-	FOREIGN KEY (facName) REFERENCES Faculty(facName)
+	PRIMARY KEY(presentationID)
 );
+
+ALTER TABLE Faculty ADD CONSTRAINT FK_Faculty_DepartmentCode FOREIGN KEY (departmentCode) REFERENCES Department(departmentCode);
+-- ALTER TABLE Faculty ADD CONSTRAINT FK_Faculty_UserID FOREIGN KEY (userID) REFERENCES Users(userID);
+ALTER TABLE Faculty ADD CONSTRAINT FK_InterviewID FOREIGN KEY (interviewID) REFERENCES Interview(interviewID);
+ALTER TABLE Faculty ADD CONSTRAINT FK_PresentationID FOREIGN KEY (presentationID) REFERENCES Presentation(presentationID);
+
+-- ALTER TABLE Student ADD CONSTRAINT FK_Student_UserID FOREIGN KEY (userID) REFERENCES Users(userID);
+ALTER TABLE Student ADD CONSTRAINT FK_Student_TopicID FOREIGN KEY (topicID) REFERENCES Topic(topicID);
+
+ALTER TABLE Topic ADD CONSTRAINT FK_Topic_FacID FOREIGN KEY (facID) REFERENCES Faculty(userID);
+
+ALTER TABLE Interview ADD CONSTRAINT FK_Int_facName FOREIGN KEY (facName) REFERENCES Faculty(facName);
+
+ALTER TABLE Presentation ADD CONSTRAINT FK_Pres_facName FOREIGN KEY (facName) REFERENCES Faculty(facName);
+
