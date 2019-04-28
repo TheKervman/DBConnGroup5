07c8e056d8d082501ce7720bfe71248280dbe3dc
Kevin <thekervman@kevins-mbp.wireless.rit.edu>
2019-04-28 16:47:20 -0400

    updated switch statements

--- MVC/src/view/MainView.java
+++ MVC/src/view/MainView.java
@@ -4,58 +4,83 @@ import controller.MainController;
 import classes.Interview;
 import classes.Presentation;
 import java.util.*;
+import java.util.Scanner;
 
 public class MainView {
 
+    Scanner input = new Scanner(System.in);  // Create a Scanner object
+
     public MainView() {
-        testGuest();
+        introMessage();
+        mainUserPrompt();
+    }
+
+    private void introMessage() {
+        System.out.println("DBConn Student/Faculty Project");
+        System.out.println("Please select one of the following options to continue");
+        System.out.println("1: Continue as a guest\n2: Log in as a faculty or student\n3: Exit the program");
+        System.out.print("Please enter a number: ");
     }
    
-   public void testGuest() {
-       Scanner input = new Scanner(System.in);  // Create a Scanner object
-       System.out.println("Welcome");
+   private void mainUserPrompt() {
+       String userInput = "";
+       while ((userInput = input.nextLine()) != "") {
+           try {
+               int input = Integer.parseInt(userInput);
 
-       int numInput = input.nextInt();
+                //checks
+                switch (input) {
+                   case 1:
+                       System.out.println("Welcome Guest");
+                       Scanner guestInput = new Scanner(System.in);
+                       int options = guestInput.nextInt();
+                       guestPrompt();
+                       break;
 
-       //checks
-       switch (numInput) {
-           case 1:
-               System.out.println("Welcome Guest");
-               Scanner guestInput = new Scanner(System.in);
-               int options = guestInput.nextInt();
-               switch (guestInput) {
-                   case 4:
-                       System.out.println("Showing all topics");
+                   case 2:
+                       System.out.println("Please Log in");
+                       break;
 
-                   case 5:
-                       System.out.println("Enter your name: ");
-                       String Name = Scanner.nextLine();
+                   case 3:
+                       System.out.println("Thank you for using this program.");
+                       System.exit(0);
 
-                       System.out.println("Enter the date: ");
-                       String Date = Scanner.nextLine();
+                   default:
+                       System.out.println("Not a valid number.");
+                       break;
+                } // end of main switch
+                System.out.println("\n");
+                introMessage();
+           } catch (NumberFormatException nfe) {
+               System.out.println("Please enter a valid number");
+           } // end of try/catch
 
-                       System.out.println("Enter the faculty member: ");
-                       String facMember = Scanner.nextLine();
+           int numInput = input.nextInt();
+       }
 
-                       String sql2 = "INSERT INTO Interview (interviewName, interviewDate, facultyMemeber) VALUES ('" + Name + "','" + Date + "','" + facMember + "');";
-                       Interview.insertQuery(sql2);
 
-                       String sql3 = "INSERT INTO Presentation (presentationName, presentationDate, facultyMemeber) VALUES ('" + Name + "','" + Date + "','" + facMember + "');";
-                       Presentation.insertQuery(sql3);
-               } // end of guest switch
-               break;
+   } // end of main user prompt
 
-           case 2:
-               System.out.println("Please Log in");
-               break;
+    private void guestPrompt() {
+        switch (guestInput) {
+            case 4:
+                System.out.println("Showing all topics");
 
-           case 3:
-               System.out.println("Thank you for using this program.");
-               System.exit(0);
+            case 5:
+                System.out.println("Enter your name: ");
+                String Name = Scanner.nextLine();
 
-           default:
-               System.out.println("Not a valid number.");
-               break;
-       } // end of main switch
-   } // end of testGuest
+                System.out.println("Enter the date: ");
+                String Date = Scanner.nextLine();
+
+                System.out.println("Enter the faculty member: ");
+                String facMember = Scanner.nextLine();
+
+                String sql2 = "INSERT INTO Interview (interviewName, interviewDate, facultyMemeber) VALUES ('" + Name + "','" + Date + "','" + facMember + "');";
+                Interview.insertQuery(sql2);
+
+                String sql3 = "INSERT INTO Presentation (presentationName, presentationDate, facultyMemeber) VALUES ('" + Name + "','" + Date + "','" + facMember + "');";
+                Presentation.insertQuery(sql3);
+        } // end of guest switch
+    }
 } // end of main view
