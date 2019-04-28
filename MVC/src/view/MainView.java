9171e2ca374c4c689982e26de9bb257c64adba96
Kevin <thekervman@kevins-mbp.wireless.rit.edu>
2019-04-28 16:03:42 -0400

    made main view readable

--- MVC/src/view/MainView.java
+++ MVC/src/view/MainView.java
@@ -11,44 +11,51 @@ public class MainView {
         testGuest();
     }
    
-   public void testGuest(){
-    Scanner input = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Welcome");
+   public void testGuest() {
+       Scanner input = new Scanner(System.in);  // Create a Scanner object
+       System.out.println("Welcome");
 
-    int numInput = input.nextInt();
-    
-    //checks
-    switch(numInput){
-      case 1:
-      System.out.println("Welcome Guest");
-          Scanner guestInput = new Scanner(System.in); 
-          int options = guestInput.nextInt();
-          switch(guestInput){
-            case 4:          
-               System.out.println("Showing all topics");
+       int numInput = input.nextInt();
 
-            case 5:
-               System.out.println("Enter your name: ");
-               String Name = Scanner.nextLine();
+       //checks
+       switch (numInput) {
+           case 1:
+               System.out.println("Welcome Guest");
+               Scanner guestInput = new Scanner(System.in);
+               int options = guestInput.nextInt();
+               switch (guestInput) {
+                   case 4:
+                       System.out.println("Showing all topics");
 
-               System.out.println("Enter the date: ");
-               String Date = Scanner.nextLine();
+                   case 5:
+                       System.out.println("Enter your name: ");
+                       String Name = Scanner.nextLine();
 
-               System.out.println("Enter the faculty member: ");
-               String facMember = Scanner.nextLine();
+                       System.out.println("Enter the date: ");
+                       String Date = Scanner.nextLine();
 
-               String sql2 = "INSERT INTO Interview (interviewName, interviewDate, facultyMemeber) VALUES ('"+Name+"','"+Date+"','"+facMember+"');";
-               Interview.insertQuery(sql2);
+                       System.out.println("Enter the faculty member: ");
+                       String facMember = Scanner.nextLine();
 
-               String sql3 = "INSERT INTO Interview (interviewName, interviewDate, facultyMemeber) VALUES ('"+Name+"','"+Date+"','"+facMember+"');";
-               Presentation.insertQuery(sql3);
-          }
-      break;
-      
-       case 2:
-      System.out.println("Please Log in");
-      break;
-     }
-   }
+                       String sql2 = "INSERT INTO Interview (interviewName, interviewDate, facultyMemeber) VALUES ('" + Name + "','" + Date + "','" + facMember + "');";
+                       Interview.insertQuery(sql2);
 
+                       String sql3 = "INSERT INTO Interview (interviewName, interviewDate, facultyMemeber) VALUES ('" + Name + "','" + Date + "','" + facMember + "');";
+                       Presentation.insertQuery(sql3);
+               }
+               break;
+
+           case 2:
+               System.out.println("Please Log in");
+               break;
+
+           case 3:
+               System.out.println("Thank you for using this program.");
+               System.exit(0);
+
+           default:
+               System.out.println("Not a valid number.");
+               break;
+       } // end of switch
+   } // end of testGuest
 } // end of main view
