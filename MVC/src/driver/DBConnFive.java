8632bad6d0e89456a8f301ccdeb2f06675f7b7f2
rxt3820 <rxt3820@rit.edu>
2019-04-29 18:23:44 -0400

    moved files and driver working

--- MVC/src/driver/DBConnFive.java
+++ MVC/src/driver/DBConnFive.java
@@ -1,3 +1,4 @@
+package driver;
 import java.sql.*;
 import java.util.ArrayList;
 
--- MVC/src/view/MainView.java
+++ MVC/src/view/MainView.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.MainController;
+import driver.DBConnFive;
 //import classes.Interview;
 //import classes.Presentation;
 import java.util.*;
@@ -15,7 +16,6 @@ public class MainView {
 
     // Messages
     private void introMessage() {
-        System.out.println("\n");
         System.out.println("DBConn Student/Faculty Project");
         System.out.println("Please select one of the following options to continue");
         System.out.println("1: Continue as a guest\n2: Log in as a faculty or student\n3: Exit the program");
@@ -23,12 +23,32 @@ public class MainView {
     }
 
     private void guestMessage() {
-        System.out.println("\n");
         System.out.println("Welcome to the Guest Menu");
         System.out.println("Please select one of the following options to continue");
         System.out.println("1: Display all topics\n2: Set up an interview\n3: Set up a presentation\n4: Go back to the main menu\n5: Exit the program");
         System.out.print("Please enter a number: ");
     }
+
+    private void loginMessage() {
+        System.out.println("Welcome to the Login Menu");
+        System.out.println("Please enter your login credentials");
+    }
+
+    private void facultyMessage() {
+        System.out.println("Welcome to the Faculty Menu");
+        System.out.println("Please select one of the following options to continue");
+        System.out.println("1: Display all topics you own\n2: Edit your topics\n3: View interviews and presentations assigned to you\n4: Logout");
+        System.out.print("Please enter a number: ");
+    }
+
+    private void studentMessage() {
+        System.out.println("Welcome to the Student Menu");
+        System.out.println("Please select one of the following options to continue");
+        System.out.println("1: List all topics\n2: List all topics you've shown interest in\n3: Logout");
+        System.out.print("Please enter a number: ");
+    }
+
+
 
     // User Prompts
     private void mainUserPrompt() {
@@ -47,21 +67,26 @@ public class MainView {
                        break;
 
                    case 2:
-                       System.out.println("Please Log in");
+                       clearScreen();
+                       loginMessage();
+                       loginPrompt();
                        break;
 
                    case 3:
+                       clearScreen();
                        System.out.println("Thank you for using this program.");
                        System.exit(0);
 
                    default:
                        System.out.println("Not a valid number.");
+                       System.out.println("Please enter a valid number: ");
                        break;
                 } // end of main switch
                 System.out.println("\n");
                 introMessage();
            } catch (NumberFormatException nfe) {
-               System.out.println("Please enter a valid number");
+               System.out.println("\n");
+               System.out.println("Please enter a valid number: ");
            } // end of try/catch
        } // end of while
 
@@ -78,6 +103,8 @@ public class MainView {
                 switch (guestInput2) {
                     case 1:
                         System.out.println("Showing all topics");
+                        DBConnFive drive = new DBConnFive();
+                        drive.getData("SELECT topicName FROM Topic");
                         break;
 
                     case 2:
@@ -111,28 +138,139 @@ public class MainView {
                         break;
 
                     case 4:
+                        clearScreen();
                         introMessage();
                         mainUserPrompt();
                         break;
 
                     case 5:
+                        clearScreen();
                         System.out.println("Thank you for using this program");
                         System.exit(1);
 
                     default:
                         System.out.println("Not a valid number.");
+                        System.out.println("Please enter a valid number: ");
                         break;
                 } // end of guest switch
                 System.out.println("\n");
                 guestMessage();
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter a valid number");
+                System.out.println("\n");
+                System.out.print("Please enter a valid number: ");
             } // end of try/catch
         } // end of while
-    } // end of guestPrompt
+    } // end of guest prompt
+
+    private void loginPrompt() {
+        String userName = "test";
+        String password = "password";
+
+        System.out.print("Please enter your username: ");
+        Scanner name = new Scanner(System.in);
+        String uName = name.nextLine();
+
+        System.out.print("Please enter your password: ");
+        Scanner pass = new Scanner(System.in);
+        String pWord = pass.nextLine();
+
+        if (uName.equals(userName) && pWord.equals(password)) {
+            System.out.println("Login successfull!");
+            clearScreen();
+            facultyMessage();
+            facultyPrompt();
+        } else {
+            System.out.println("Incorrect username or password, please try again.");
+        }
+
+    } // end of login prompt
+
+    private void facultyPrompt() {
+        Scanner input = new Scanner(System.in);
+        String facInput = "";
+        while ((facInput = input.nextLine()) != "") {
+            try {
+                int facInput2 = Integer.parseInt(facInput);
+
+                //checks
+                switch (facInput2) {
+                    case 1:
+                        System.out.println("List all topics");
+                        break;
+
+                    case 2:
+                        System.out.println("Edit topics (test student prompt)");
+                        clearScreen();
+                        studentMessage();
+                        studentPrompt();
+                        break;
+
+                    case 3:
+                        System.out.println("View interviews and presentations");
+                        break;
+
+                    case 4:
+                        clearScreen();
+                        introMessage();
+                        mainUserPrompt();
+                        break;
+
+                    default:
+                        System.out.println("Not a valid number.");
+                        System.out.println("Please enter a valid number: ");
+                        break;
+                } // end of faculty switch
+                System.out.println("\n");
+                facultyMessage();
+            } catch (NumberFormatException nfe){
+                System.out.println("\n");
+                System.out.print("Please enter a valid number: ");
+            } // end of try/catch
+        } // end of while
+    } // end of faculty prompt
+
+    private void studentPrompt() {
+        Scanner input = new Scanner(System.in);
+        String studInput = "";
+        while ((studInput = input.nextLine()) != "") {
+            try {
+                int studInput2 = Integer.parseInt(studInput);
+
+                //checks
+                switch (studInput2) {
+                    case 1:
+                        System.out.println("List all topics");
+                        break;
+
+                    case 2:
+                        System.out.println("List topics you're interested in");
+                        break;
+
+                    case 3:
+                        clearScreen();
+                        introMessage();
+                        mainUserPrompt();
+                        break;
+
+                    default:
+                        System.out.println("Not a valid number.");
+                        System.out.println("Please enter a valid number: ");
+                        break;
+                } // enf of student switch
+                System.out.println("\n");
+                studentMessage();
+            } catch (NumberFormatException nfe) {
+                System.out.println("\n");
+                System.out.println("Please enter a valid number: ");
+            } // enf of try/catch
+        } // end of while
+    } // end of student prompt
 
     // Clear Screen
     public static void clearScreen() {
+        System.out.println("\n");
+        System.out.println("\n");
+        System.out.println("\n");
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
